f8d651b5ec6b8489f59cb2388a446c96e304064f
Yuffie <441843578@qq.com>
2018-05-11 13:59:09 +0800

    新增loading页面

---
+++ page/loading/loading.js
@@ -0,0 +1,100 @@
+const app = getApp();
+const Data = require('../../utils/data/data');
+const data = require('../../utils/data');
+Page({
+  data: {
+  },
+  onLoad: function (options) {
+    let that = this;
+    Promise.all([this.reqGoods(), this.reqCategories(), this.reqDiscount()]).then(function (res) {
+      let goods = res[0];
+      let discounts = res[2];
+      if(discounts['CU_XIAO']){
+        Object.keys(discounts['CU_XIAO'].items).forEach((id)=>{
+          goods[id].sale_price = discounts['CU_XIAO'].items[id].sale_price;
+        });
+      }
+      app.globalData.categories = res[1];
+      app.globalData.goods = goods;
+      app.globalData.discounts = discounts;
+      wx.redirectTo({
+        url: '../shop/shop'
+      })
+    }, function (res) {
+      console.log(res);
+    });
+  },
+  onShow: function () {
+
+  },
+
+  /*获取品类信息*/
+  reqCategories(){
+    return new Promise((resolve, reject)=>{
+      Data.getCategories({
+        success_0(res){
+          resolve(res);
+        },
+        success_1(res){
+          reject(res.msg || '未获取到品类信息');
+        }
+      })
+    })
+  },
+
+  /*获取促销信息*/
+  reqDiscount(){
+    let that = this;
+    return new Promise((resolve, reject)=>{
+      let discounts = data.discounts;
+      if(discounts){
+        discounts = this.formatDiscount(discounts);
+        resolve(discounts);
+      } else {
+        reject('未获取到促销信息')
+      }
+    });
+
+  },
+  formatDiscount(discounts){
+    let obj = {};
+    discounts.map((discount)=>{
+      if(discount.type === 'CU_XIAO'){
+        let obj = {};
+        discount.items.map((item)=>{
+          obj[item.item_id] = item;
+        });
+        discount.items = obj;
+      }
+      obj[discount.type] = discount;
+    });
+    return obj;
+  },
+
+
+  /*获取商品信息*/
+  reqGoods(){
+    let that = this;
+    return new Promise((resolve, reject)=>{
+      Data.getGoods({
+        success_0(res){
+          let goods = that.formatGoodsData(res);
+          resolve(goods);
+        },
+        success_1(res){
+          reject(res.msg || '未获取到商品信息');
+        }
+      })
+    })
+  },
+  formatGoodsData(goods){
+    let obj = {};
+    goods.map((good)=>{
+      good.pic = '../../images/web/' + good.image + '.jpg';
+      delete good.image;
+      obj[good.id] = good;
+    });
+    return obj;
+  },
+});
+
